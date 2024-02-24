f09afb91a2007686811cee7044e17615874fb4a2
Jatinsinghsaini2100 <128669990+Jatinsinghsaini2100@users.noreply.github.com>
2024-02-24 22:52:03 +0530

    Update afk.py

--- Yukki/modules/afk.py
+++ Yukki/modules/afk.py
@@ -190,7 +190,7 @@ from Yukki.database import add_afk, is_afk, remove_afk
 from Yukki.helpers import get_readable_time, put_cleanmode
 
 
-@app.on_message(filters.command(["afk", f"afk@{botusername}"]) & ~filters.edited)
+@app.on_message(filters.command(["afk", f"afk@{botusername}"]) & ~filters.forwarded)
 async def active_afk(_, message: Message):
     if message.sender_chat:
         return
